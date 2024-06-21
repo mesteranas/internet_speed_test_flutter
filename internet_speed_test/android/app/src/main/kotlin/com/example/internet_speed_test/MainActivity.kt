051b01fe45cdb96cf06759587ef01e989e047594
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-06-21 19:19:04 +0300

    21/06/2024

---
+++ internet_speed_test/android/app/src/main/kotlin/com/example/internet_speed_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.internet_speed_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ internet_speed_test/lib/app.dart
@@ -0,0 +1,7 @@
+class App{
+  static const name="internet speed test";
+  static const appName="internet_speed_test_flutter";
+  static const appDirName="internet_speed_test";
+  static const version=1.0;
+  static const description="a app to test your internet speed";
+}
